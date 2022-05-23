f86280d4ea5147163d18283716e23ab547ae6736
RodrigoMontero1998 <46728177+RodrigoMontero1998@users.noreply.github.com>
2022-05-23 11:24:10 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto1/Nuevo.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Proyecto1
+{
+    partial class Nuevo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ConsolidateBtn = new System.Windows.Forms.Button();
+            this.ChoiseFolderBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPath = new System.Windows.Forms.TextBox();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // ConsolidateBtn
+            // 
+            this.ConsolidateBtn.Location = new System.Drawing.Point(686, 451);
+            this.ConsolidateBtn.Name = "ConsolidateBtn";
+            this.ConsolidateBtn.Size = new System.Drawing.Size(150, 31);
+            this.ConsolidateBtn.TabIndex = 0;
+            this.ConsolidateBtn.Text = "Consolidar";
+            this.ConsolidateBtn.UseVisualStyleBackColor = true;
+            this.ConsolidateBtn.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // ChoiseFolderBtn
+            // 
+            this.ChoiseFolderBtn.Location = new System.Drawing.Point(692, 362);
+            this.ChoiseFolderBtn.Name = "ChoiseFolderBtn";
+            this.ChoiseFolderBtn.Size = new System.Drawing.Size(144, 31);
+            this.ChoiseFolderBtn.TabIndex = 1;
+            this.ChoiseFolderBtn.Text = "Seleccionar carpeta";
+            this.ChoiseFolderBtn.UseVisualStyleBackColor = true;
+            this.ChoiseFolderBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Directorio";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textPath
+            // 
+            this.textPath.Location = new System.Drawing.Point(93, 366);
+            this.textPath.Name = "textPath";
+            this.textPath.ReadOnly = true;
+            this.textPath.Size = new System.Drawing.Size(593, 22);
+            this.textPath.TabIndex = 3;
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.LargeImageList = this.imageList;
+            this.listView.Location = new System.Drawing.Point(15, 12);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(843, 315);
+            this.listView.TabIndex = 5;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Nuevo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 508);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.textPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ChoiseFolderBtn);
+            this.Controls.Add(this.ConsolidateBtn);
+            this.Name = "Nuevo";
+            this.Text = "Consolidador de Excel";
+            this.Load += new System.EventHandler(this.Nuevo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConsolidateBtn;
+        private System.Windows.Forms.Button ChoiseFolderBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textPath;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ Proyecto1/Nuevo.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Xls;
+
+namespace Proyecto1
+{
+    public partial class Nuevo : Form
+    {
+        public Nuevo()
+        {
+            InitializeComponent();
+        }
+
+        private void Nuevo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<String> xlsfiles= new List<string>();
+        private List<String> othersfiles = new List<string>();
+
+        private void button1_ClickAsync(object sender, EventArgs e)
+        {
+
+            string aux = procesar(textPath.Text, xlsfiles);
+            if (aux != "")
+            {
+                string dir = textPath.Text;
+                string dirxls = createFolder(dir, "Procesado");
+                string dirotherfiles = createFolder(dir, "No aplicable");
+                foreach (string elem in xlsfiles)
+                {
+                    moveFile(elem, dirxls);
+                }
+                foreach (string elem in othersfiles)
+                {
+                    moveFile(elem, dirotherfiles);
+                }
+                //resetear para que vuelvan aparecer los archivos
+                //
+                listView.Items.Clear();
+                foreach (string Item in Directory.GetFiles(textPath.Text))
+                {
+                    FileInfo fi = new FileInfo(Item);
+                    imageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName));
+                    if (fi.FullName.Contains(".xls"))
+                    {
+                        xlsfiles.Add(fi.Name);
+                    }
+                    else
+                    {
+                        othersfiles.Add(fi.Name);
+                    }
+                    listView.Items.Add(fi.Name, imageList.Images.Count - 1);
+                }
+                MessageBox.Show("Se consolidaron correctamente los archivos");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true; 
+            DialogResult result = folderDlg.ShowDialog();
+             xlsfiles = new List<string>();
+            othersfiles = new List<string>();
+            listView.Items.Clear();
+            if (result == DialogResult.OK)
+            {
+                textPath.Text = folderDlg.SelectedPath;
+                foreach (string Item in Directory.GetFiles(folderDlg.SelectedPath))
+                {
+                    FileInfo fi = new FileInfo(Item);
+                    imageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName));
+                    if (fi.FullName.Contains(".xls"))
+                    {
+                        xlsfiles.Add(fi.Name);
+                    }
+                    else
+                    {
+                        othersfiles.Add(fi.Name);
+                    }
+                    listView.Items.Add(fi.Name, imageList.Images.Count-1);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private string createFolder(string dir, string nameFolder)
+        {
+            System.IO.Directory.CreateDirectory(dir+"\\"+nameFolder);
+            return dir +"\\"+ nameFolder;
+        }
+        private void moveFile(string nameFile,string dir)
+        {
+            if (!System.IO.File.Exists(dir+"\\"))
+            {
+                System.IO.File.Move(textPath.Text+"\\"+nameFile, dir+"\\"+nameFile);
+            }
+               
+        }
+
+        public string procesar(string dir, List<string> listFiles)
+        {
+            
+            List<string> inputFiles = new List<string>();
+            foreach(string item in listFiles){
+                inputFiles.Add(dir + "\\" + item);
+            }
+            if (inputFiles.Count > 0)
+            {
+                Workbook newWorkbook = new Workbook();
+                newWorkbook.Worksheets.Clear();
+                Workbook tempWorkbook = new Workbook();
+                foreach (string file in inputFiles)
+                {
+                    tempWorkbook.LoadFromFile(file);
+                    foreach (Worksheet sheet in tempWorkbook.Worksheets)
+                    {
+                        newWorkbook.Worksheets.AddCopy(sheet, WorksheetCopyType.CopyAll);
+                    }
+                }
+                
+                newWorkbook.SaveToFile(dir + "\\ArchivoMaestro.xlsx", ExcelVersion.Version2013);
+
+                return dir + "\\ArchivoMaestro.xlsx";
+                
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+       
+
+    }
+}
